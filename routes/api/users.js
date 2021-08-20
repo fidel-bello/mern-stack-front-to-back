275051c9c380020b393a02e30386b726fc8ef58e
fidel bello <59423@bears.smcc.edu>
2021-08-20 11:09:13 -0400

    password hash save user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator/check');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs')
+
 //@routes POST api/users
 //@desc   Register User
 //@access Public
@@ -14,13 +18,44 @@ router.post(
       'please enter password longer than 6 characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User route');
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json(errors[{ msg: 'user already exists' }]);
+      }
+      
+      const avatar = gravatar.url(email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+      })
+    
+      user = new User({
+          name,
+          email,
+          avatar,
+          password
+      });
+     
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+     await user.save();
+     
+      //return jsonwebtoken
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error');
+    }
   }
 );
 
