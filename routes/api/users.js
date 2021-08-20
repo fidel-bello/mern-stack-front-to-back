9cc3001d2264c686c04527c47d965899db3ef118
fidel bello <59423@bears.smcc.edu>
2021-08-20 11:18:25 -0400

    message user  already exists

--- routes/api/users.js
+++ routes/api/users.js
@@ -28,7 +28,9 @@ router.post(
     try {
       let user = await User.findOne({ email });
       if (user) {
-        res.status(400).json(errors[{ msg: 'user already exists' }]);
+        res
+        .status(400)
+        .json({ errors: [{ msg: 'user already exists' }]});
       }
       
       const avatar = gravatar.url(email, {
@@ -49,9 +51,9 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
      await user.save();
-     
+
       //return jsonwebtoken
-      res.send('User route');
+      res.send('User registered!');
     } catch (err) {
       console.error(err.message);
       res.status(500).send('server error');
