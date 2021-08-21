c7369ec061e8b838c32ac9399b67dd57fdc9cbba
fidel bello <59423@bears.smcc.edu>
2021-08-21 10:48:20 -0400

    making auth route respond back the user data

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User')
 //@routes GET api/auth
 //@desc   Test route
 //@access Public
-router.get('/', (req,res) => res.send('auth route'));
+router.get('/', auth, async (req,res) => {
+try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user)
+} catch (error) {
+    console.error(error.message);
+    res.status(500).send('server error')
+}
+});
 
 module.exports = router;
