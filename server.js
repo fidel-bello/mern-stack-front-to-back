d982cd7e971c957cdc791aba8c7bd78dcf820641
fidel bello <59423@bears.smcc.edu>
2021-08-23 00:23:49 -0400

    finally?

--- server.js
+++ server.js
@@ -25,3 +25,5 @@ app.use('/api/posts', require('./routes/api/posts'))
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+
+//finally?
