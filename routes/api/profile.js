b9b14f5aa85df0d3618c18c5743fbbb60d92ec44
fidel bello <59423@bears.smcc.edu>
2021-08-22 23:26:10 -0400

    add experience to resume

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -164,7 +164,57 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+//@routes PUT api/profile/experience
+//@desc  add profile experience
+//@access private
 
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ error: errors.array() });
+    }
+
+    const { title, company, location, from, to, current, description } =
+      req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+//@routes DELETE api/profile/experience/:exp_id
+//@desc  delete experience from profile
+//@access private
 
 module.exports = router;
 //testing commit
