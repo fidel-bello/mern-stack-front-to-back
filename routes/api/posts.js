c0f462c2dba0b838eb5746756294a0af07ceb8af
fidel bello <59423@bears.smcc.edu>
2021-08-23 23:56:08 -0400

    unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -97,7 +97,7 @@ router.delete('/:id', auth, async (req, res) => {
 });
 
 //@routes PUT api/posts/like/:id
-//@desc   like a post 
+//@desc   like a post
 //@access Private
 router.put('/like/:id', auth, async (req, res) => {
   try {
@@ -113,6 +113,36 @@ router.put('/like/:id', auth, async (req, res) => {
     post.likes.unshift({ user: req.user.id });
 
     await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+});
+
+//@routes PUT api/posts/unlike/:id
+//@desc   like a post
+//@access Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    //check if the post has already been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'post has not yet been liked' });
+    }
+
+    //get remove index
+    const removeindex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeindex, 1);
+
+    await post.save();
+
     res.json(post.likes);
   } catch (err) {
     console.error(err.message);
