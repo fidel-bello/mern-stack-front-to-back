5920511823253b55ab3da25010ac480101a8d558
fidel bello <59423@bears.smcc.edu>
2021-08-24 00:29:45 -0400

    post a comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,8 +10,7 @@ const User = require('../../models/User');
 
 router.post(
   '/',
-  auth,
-  check('text', 'Text is required').notEmpty(),
+  [auth, [check('text', 'Text is required').notEmpty()]],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -149,5 +148,45 @@ router.put('/unlike/:id', auth, async (req, res) => {
     res.status(500).send('server error');
   }
 });
+
+//@routes POST api/posts/comment/:id
+//@desc   comment on post 
+//@access Private
+
+router.post(
+  '/comment/:id',
+  [ 
+    auth,
+    [ 
+  check('text', 'Text is required').notEmpty(),
+  ]
+], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+       await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
