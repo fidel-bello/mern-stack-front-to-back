98a6c72ce939781b864ee5da1954130e6276fa79
fidel bello <59423@bears.smcc.edu>
2021-08-26 00:28:41 -0400

    copy paste same code from register.js to login.js but will change later

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 export const Register = () => {
@@ -14,29 +13,12 @@ export const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
   
-    const onSubmit =  async e => {
+    const onSubmit = e => {
         e.preventDefault();
         if(password !== password2) {
             console.log('passwords do not match')
         } else {
-           const newUser = {
-             name,
-             email,
-             password
-           }
-           try {
-             const config = {
-               headers: {
-                 'Content-Type': 'application/json'
-               }
-             }
-             const body = JSON.stringify(newUser);
-             const res = await axios.post('/api/users', body, config);
-
-             console.log(res.data);
-           } catch (err) {
-             console.error(err.response.data)
-           }
+            console.log(formData)
         }
     };
   return (
