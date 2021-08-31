57cea0f591e366ec07552f7b5139339e33b9feba
fidel bello <59423@bears.smcc.edu>
2021-08-31 10:13:24 -0400

    deleted props.setalert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
 
-const Register = (props) => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -18,7 +18,7 @@ const Register = (props) => {
     const onSubmit = e => {
         e.preventDefault();
         if(password !== password2) {
-            props.setAlert('passwords do not match', 'danger')
+            setAlert('passwords do not match', 'danger')
         } else {
             console.log(formData)
         }
