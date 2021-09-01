0f63a84b4c7fd455ee52692bf6e85a2ab5942d09
fidel bello <59423@bears.smcc.edu>
2021-09-01 10:14:23 -0400

    imported types into  actions/auth.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL,  USER_LOADED, AUTH_ERROR } from './types';
 
 //Register user
 
