0c70e2ab2ebf431e6f56f7c9bc5dd8bb0006e2ff
fidel bello <59423@bears.smcc.edu>
2021-09-01 10:19:36 -0400

    check for token in local storage set it to token for global header else, delete from global header

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import { REGISTER_SUCCESS, REGISTER_FAIL,  USER_LOADED, AUTH_ERROR } from './types';
 
-//Register user
+//load user
+export const loadUser = () => async dispatch => {
+  
+}
 
+//Register user
 export const register =
   ({ name, email, password }) =>
   async (dispatch) => {
