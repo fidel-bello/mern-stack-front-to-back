d355e808ef450df79ef2f7f6488444f9fae71cb8
fidel bello <59423@bears.smcc.edu>
2021-09-02 13:14:45 -0400

    if user tries to go to dashboard without authentication, they will be redirected to sign it

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 //redux
 import { Provider } from 'react-redux';
@@ -32,7 +33,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
-              <Route exact path= '/dashboard' component={Dashboard} />
+              <PrivateRoute exact path= '/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
