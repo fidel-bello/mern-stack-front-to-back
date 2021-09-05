ae24172efe45f8e290427bb6c7db17be246fd773
fidel bello <59423@bears.smcc.edu>
2021-09-04 20:49:04 -0400

    post for user profile when not created

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,24 +1,56 @@
-import axios from 'axios'
-import {setAlert} from './alert';
-import {
-    GET_PROFILE,
-    PROFILE_ERROR
-} from './types';
+import axios from 'axios';
+import { set } from 'mongoose';
+import { setAlert } from './alert';
+import { GET_PROFILE, PROFILE_ERROR } from './types';
 
 //get current users profile
 
-export const getCurrentProfile = () =>  async dispatch => {
-    try {
-        const res = await axios.get('/api/profile/me');
+export const getCurrentProfile = () => async (dispatch) => {
+  try {
+    const res = await axios.get('/api/profile/me');
 
-        dispatch ({
-            type: GET_PROFILE,
-            payload: res.data
-        });
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//create or update profile
+export const createProfile =
+  (formData, history, edit = false) =>
+  async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      const res = await axios.post('/api/profile', formData, config);
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+
+      if (!edit) {
+        history.push('/dashboard');
+      }
     } catch (err) {
-        dispatch({
-            type: PROFILE_ERROR,
-            payload: { msg: err.response.statusText, status: err.response.status }
-        })
+      if (err) {
+        err.forEach((err) => dispatch(setAlert(err.msg, 'danger')));
+      }
+
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      });
     }
-}
+  };
