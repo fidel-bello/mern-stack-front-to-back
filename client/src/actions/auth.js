64a2f832a2f231fcd92994ee72e9c429ff51504f
fidel bello <59423@bears.smcc.edu>
2021-09-05 22:51:03 -0400

    format

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,7 +8,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-  CLEAR_PROFILE
+  CLEAR_PROFILE,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 //load user
@@ -60,38 +60,36 @@ export const register =
   };
 
 //login
-  export const login =
-  (  email, password) =>
-  async (dispatch) => {
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
-
-    const body = JSON.stringify({ email, password });
-    try {
-      const res = await axios.post('api/auth', body, config);
-      dispatch({
-        type: LOGIN_SUCCESS,
-        payload: res.data,
-      });
-
-      dispatch(loadUser())
-    } catch (err) {
-      const errors = err.response.data.errors;
-      if (errors) {
-        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-      }
-      dispatch({
-        type: LOGIN_FAIL,
-      });
-    }
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
   };
 
-// Logout / clear profile 
+  const body = JSON.stringify({ email, password });
+  try {
+    const res = await axios.post('api/auth', body, config);
+    dispatch({
+      type: LOGIN_SUCCESS,
+      payload: res.data,
+    });
 
-export const logout = () =>dispatch => {
-  dispatch({ type: CLEAR_PROFILE});
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
+    dispatch({
+      type: LOGIN_FAIL,
+    });
+  }
+};
+
+// Logout / clear profile
+
+export const logout = () => (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
