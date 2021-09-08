dccc3d22646f2cea1232578231f2629c5e0eb9c4
fidel bello <59423@bears.smcc.edu>
2021-09-08 11:53:39 -0400

    added delete posts for the back end so everything deletes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Posr = require('../../models/Posts');
 const { check, validationResult } = require('express-validator/check');
 const request = require('request');
 const config = require('config');
@@ -150,6 +151,9 @@ router.get('/user/:user_id', async (req, res) => {
 
 router.delete('/', auth, async (req, res) => {
   try {
+    //remove user posts
+    await Post.deleteMAny({ user: req.user.id });
+
     //remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
