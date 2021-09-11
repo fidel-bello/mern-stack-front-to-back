97655133e25c2674e8dbcdfbf3c63e173bc54d16
fidel bello <59423@bears.smcc.edu>
2021-09-10 22:50:07 -0400

    map works but returning test divs as of now but is getting all profiles in the back end

---
+++ client/src/components/profiles/Profileitem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Profileitem = props => {
+    return (
+        <div>
+            test
+        </div>
+    )
+}
+
+Profileitem.propTypes = {
+
+}
+
+export default Profileitem
--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import Profileitem from './Profileitem';
 import { getProfiles } from '../../actions/profile';
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
@@ -9,7 +10,24 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
     getProfiles();
   }, []);
 
-  return <div></div>;
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Developers</h1>
+      <p className='lead'>
+        <i className='fab fa-connectdevelop'></i> Browse and connect with
+        developers
+      </p>
+      <div className='profiles'>
+        {profiles.length > 0 ? (
+          profiles.map((profile) => (
+            <Profileitem key={profile._id} profile={profile} />
+          ))
+        ) : (
+          <h4>No profiles found...</h4>
+        )}
+      </div>
+    </Fragment>
+  );
 };
 
 Profiles.propTypes = {
