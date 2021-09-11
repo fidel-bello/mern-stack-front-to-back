957f5f9a155be56923fd4840fe3d853346e0d0cc
fidel bello <59423@bears.smcc.edu>
2021-09-11 09:18:13 -0400

    setting up profile route for get profiles by id when view profile is clicked

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,18 @@
+import React, {Fragment, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import { connect} from 'react-redux';
+import { getProfileById } from '../../actions/profile';
+
+const Profile = props => {
+    return (
+        <div>
+            Profile
+        </div>
+    )
+}
+
+Profile.propTypes = {
+
+}
+
+export default connect(mapStateToProps, {getProfileById})(Profile);
