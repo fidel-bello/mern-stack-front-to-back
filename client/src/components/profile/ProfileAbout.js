feb85d914ee5214164a7133cb9bff92f75567cd8
fidel bello <59423@bears.smcc.edu>
2021-09-14 19:31:33 -0400

    check to see if profile has bio and also split name and got the first name [0]

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const ProfileAbout = ({
@@ -7,10 +7,42 @@ const ProfileAbout = ({
     skills,
     user: { name },
   },
-}) => {
-  return <div></div>;
-};
+}) => (
+  <div class='profile-about bg-light p-2'>
+    {bio && (
+      <Fragment>
+        <h2 class='text-primary'>{name.trim().split(' ')[0]}'s Bio</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed
+          doloremque nesciunt, repellendus nostrum deleniti recusandae nobis
+          neque modi perspiciatis similique?
+        </p>
+        <div class='line'></div>
+      </Fragment>
+    )}
+    <h2 class='text-primary'>Skill Set</h2>
+    <div class='skills'>
+      <div class='p-1'>
+        <i class='fa fa-check'></i> HTML
+      </div>
+      <div class='p-1'>
+        <i class='fa fa-check'></i> CSS
+      </div>
+      <div class='p-1'>
+        <i class='fa fa-check'></i> JavaScript
+      </div>
+      <div class='p-1'>
+        <i class='fa fa-check'></i> Python
+      </div>
+      <div class='p-1'>
+        <i class='fa fa-check'></i> C#
+      </div>
+    </div>
+  </div>
+);
 
-ProfileAbout.propTypes = {};
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
 
 export default ProfileAbout;
