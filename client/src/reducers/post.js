ac2bf5771c545e9dbb872a59f012c5ea1224409c
fidel bello <59423@bears.smcc.edu>
2021-09-18 18:34:01 -0400

    format

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,32 +1,29 @@
-import {
-    GET_POSTS,
-    POST_ERROR
-} from '../actions/types'
+import { GET_POSTS, POST_ERROR } from '../actions/types';
 
 const initialState = {
-    posts: [],
-    post: null,
-    loading: true,
-    error: {}
-}
+  posts: [],
+  post: null,
+  loading: true,
+  error: {},
+};
 
-export default function(state = initialState, action) {
-    const {type, payload} = action;
+export default function (state = initialState, action) {
+  const { type, payload } = action;
 
-    switch(type){
-        case GET_POSTS:
-            return {
-                ...state,
-                post: payload,
-                loading: false
-            }
-            case POST_ERROR:
-                return {
-                    ...state,
-                    error: payload,
-                    loading: false
-                }
-            default: 
-            return state;
-    }
+  switch (type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        post: payload,
+        loading: false,
+      };
+    case POST_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
 }
