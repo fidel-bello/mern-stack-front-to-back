cacc594aa8480d6786e171a27aa0f15fd8d1b98a
fidel bello <59423@bears.smcc.edu>
2021-09-19 10:29:41 -0400

    added mapstatetoprops and also added properties

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Moment from 'react-moment';
 
-const PostItem = props => {
-    return (
-        <div>
-            
-        </div>
-    )
+const PostItem = ({
+  post: { _id, text, name, avatar, user, likes, comments, date },
+  auth,
+}) => {
+  return <div></div>;
 };
 
 PostItem.propTypes = {
-
+  post: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
 
-export default connect(null, {})(PostItem)
+export default connect(mapStateToProps, {})(PostItem);
