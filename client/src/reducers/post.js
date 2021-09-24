317bf7d9863d0031e45091daa56a19f485c9edfc
fidel bello <59423@bears.smcc.edu>
2021-09-24 19:50:58 -0400

    added loading = false

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -29,6 +29,7 @@ export default function (state = initialState, action) {
         posts: state.posts.map((post) =>
           post._id === payload.id ? { ...post, likes: payload.likes } : post
         ),
+        loading: false
       };
     default:
       return state;
