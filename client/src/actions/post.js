baa8ae49108e40a25641e9283e30951abd4aa000
fidel bello <59423@bears.smcc.edu>
2021-09-24 22:58:20 -0400

    delete post;

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
 
 //get posts
 export const getPosts = () => async (dispatch) => {
@@ -50,3 +50,22 @@ export const removeLike = (id) => async (dispatch) => {
     });
   }
 };
+
+// delete post
+
+export const deletePost = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`)
+    dispatch({
+      type: DELETE_POST,
+      payload: id
+    });
+
+    dispatch(setAlert('Post removed', 'success'))
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+}
