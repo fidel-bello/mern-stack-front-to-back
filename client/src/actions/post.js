e1e46cadeb0bf485e76fed54c4badac7c63f1d29
fidel bello <59423@bears.smcc.edu>
2021-09-24 23:08:52 -0400

    created function of add post and created reducer

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from './types';
 
 //get posts
 export const getPosts = () => async (dispatch) => {
@@ -69,3 +69,27 @@ export const deletePost = id => async dispatch => {
     });
   }
 }
+
+export const addPost = (formdata) => async (dispatch) => {
+  const config = {
+    header: {
+      'Content-Type': 'application/json',
+    }
+  };
+
+  try {
+    await axios.post('/api/posts', formdata, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
