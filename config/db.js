8438c5540e68792c73f15d07412f2b47ad89faa6
fidel bello <59423@bears.smcc.edu>
2021-09-26 17:45:18 -0400

    db.js

--- config/db.js
+++ config/db.js
@@ -1,22 +1,20 @@
 const mongoose = require('mongoose');
-
 const config = require('config');
-
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false,
+      useUnifiedTopology: true,
     });
 
-    console.log('MongoDB connected!');
+    console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-
+    // Exit process with failure
     process.exit(1);
   }
 };
