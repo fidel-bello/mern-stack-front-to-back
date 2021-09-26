897ac769750b4f48c87693b0d6adb36b7f33ae11
fidel bello <59423@bears.smcc.edu>
2021-09-26 15:27:25 -0400

    added post single post page with back button

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,13 +4,23 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getPost } from '../../actions/post';
+import PostItem from '../posts/PostItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
   }, [getPost, match.params.id]);
 
-  return <div>post</div>;
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to='/posts' className='btn'>
+        Back to posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  );
 };
 Post.propTypes = {
   getPost: PropTypes.func.isRequired,
