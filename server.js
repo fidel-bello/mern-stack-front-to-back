f53a26fb66551fe19bf3cc6da1d25b825d2121c7
fidel bello <59423@bears.smcc.edu>
2021-09-26 17:43:34 -0400

    server.js

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
+   
 const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 
 const app = express();
 
+// Connect Database
 connectDB();
 
-//middleware
+// Init Middleware
 app.use(express.json());
 
-//define routes
+// Define Routes
 app.use('/api/users', require('./routes/api/users'));
-
 app.use('/api/auth', require('./routes/api/auth'));
-
 app.use('/api/profile', require('./routes/api/profile'));
-
 app.use('/api/posts', require('./routes/api/posts'));
 
-//serve static assets
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  //static folder
+  // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
