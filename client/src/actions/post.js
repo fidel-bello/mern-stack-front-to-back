51627d3b24b5d863b7c7f8528de0a7d7b078f3a8
fidel bello <59423@bears.smcc.edu>
2021-09-26 16:09:24 -0400

    comment are post correctly, now will display comments

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -121,7 +121,7 @@ export const getPost = (id) => async (dispatch) => {
 };
 
 //add comment
-export const addComment = (postId, formdata) = async dispatch => {
+/*export const addComment = (postId, formdata) = async dispatch => {
 const config = {
   headers: {
     'Content-Type' : 'application/json'
@@ -141,10 +141,34 @@ try {
     payload: { msg: err.response.statusText, status: err.response.status },
   });
 }
-} 
+} */
+
+export const addComment = (postId, formdata) => async (dispatch) => {
+  const config = {
+    header: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const res = await axios.post(`/api/posts/comment/${postId}`, formdata, config);
+
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data,
+    });
+
+    dispatch(setAlert('Comment Posted!', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 
 //delete comment
-export const deleteComment = (postId, commentId) = async dispatch => {
+/*export const deleteComment = (postId, commentId) = async dispatch => {
   try {
     const res = await axios.delete(`/api/comment/${postId}/${commentId}`)
     dispatch ({
@@ -159,4 +183,21 @@ export const deleteComment = (postId, commentId) = async dispatch => {
       payload: { msg: err.response.statusText, status: err.response.status },
     });
   }
-} 
+} */
+export const deleteComment = (postId, commentId) => async (dispatch) => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId,
+    });
+
+    dispatch(setAlert('Comment Removed!', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
